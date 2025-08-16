d80193f7220d27ea768e49f60ccdb2ecf3536113
agilestarskim <agilestarskim@gmail.com>
2025-08-16 16:39:10 +0900

    Initial Commit

---
+++ ZombieRush/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZombieRush
+//
+//  Created by 김민성 on 8/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZombieRush/ZombieRushApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZombieRushApp.swift
+//  ZombieRush
+//
+//  Created by 김민성 on 8/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ZombieRushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
